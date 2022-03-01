0fa24cdaaf8346342dffea9dab370eef0edcd07d
Keks <keks@htmlacademy.ru>
2022-03-01 17:15:55 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,19 +1,23 @@
+const MIN_PRICE = 1;
+const MAX_PRICE = 30000;
+const MIN_ROOMS = 1;
+const MAX_ROOMS = 8;
+const MIN_GUESTS = 1;
+const MAX_GUESTS = 10;
+const MIN_LAT = 35.65000;
+const MAX_LAT = 35.70000;
+const MIN_LNG = 139.70000;
+const MAX_LNG = 139.80000;
+const FLOAT = 5;
 const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const TIME = ['12:00', '13:00', '14:00'];
-const FEATURES = [
-  'wifi',
-  'dishwasher',
-  'parking',
-  'washer',
-  'elevator',
-  'conditioner',
-];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
 ];
-const SIMILAR_AD_COUNT = 10;
+
 
 // Возвращает случайное целое число из переданного диапазона включительно.
 const getRandomNumber = (a, b) => {
@@ -33,67 +37,37 @@ const getRandomFloat = (a, b, digits = 1) => {
   return +result.toFixed(digits);
 };
 
-const getRandomArrayElement = (elements) =>
-  elements[getRandomNumber(0, elements.length - 1)]; // Вовзращает случайный элемент массива
+// Вовзращает случайный элемент массива
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
 
-const usersId = Array.from({ length: SIMILAR_AD_COUNT }, (v, i) => ++i); // Генерирует последовательность чисел от 1 до SIMILAR_AD_COUNT
+const createAd = (i) => {
+  const id = `${i}`.padStart(2, '0');
+  const location = {
+    lat: getRandomFloat(MIN_LAT, MAX_LAT, FLOAT),
+    lng: getRandomFloat(MIN_LNG, MAX_LNG, FLOAT),
+  };
 
-const getRandomLengthArr = (array) => {
-  const newArr = [];
-  const count = getRandomNumber(1, array.length);
-  for (let i = 0; i < count; i++) {
-    newArr.push(array[i]);
-  }
-  return newArr;
+  return {
+    autor: {
+      avatar: `img/avatars/user${id}.png`
+    },
+    location,
+    offer: {
+      title: 'Заголовок предложения',
+      address: `${location.lat}, ${location.lng}`,
+      price: getRandomNumber(MIN_PRICE, MAX_PRICE),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomNumber(MIN_ROOMS, MAX_ROOMS),
+      guests: getRandomNumber(MIN_GUESTS, MAX_GUESTS),
+      checkin: getRandomArrayElement(TIME),
+      checkout: getRandomArrayElement(TIME),
+      features: FEATURES.slice(0, getRandomNumber(1, FEATURES.length)),
+      description: 'Описание помещения',
+      photos: PHOTOS.slice(0, getRandomNumber(1, PHOTOS.length)),
+    }
+  };
 };
 
-const getAutor = (i) => ({
-  avatar: `img/avatars/user${usersId[i]}.png`,
-});
-
-const getLocation = () => ({
-  lat: getRandomFloat(35.65, 35.7, 5),
-  lng: getRandomFloat(139.7, 139.8, 5),
-});
-
-const getOffer = () => ({
-  title: 'Заголовок предложения',
-  address: `${getLocation().lat}, ${getLocation().lng}`,
-  price: getRandomNumber(1, 30000),
-  type: getRandomArrayElement(TYPES),
-  rooms: getRandomNumber(1, 5),
-  guests: getRandomNumber(1, 8),
-  checkin: getRandomArrayElement(TIME),
-  checkout: getRandomArrayElement(TIME),
-  features: getRandomLengthArr(FEATURES),
-  description: 'Описание помещения',
-  photos: getRandomLengthArr(PHOTOS),
-});
-
-const generateAds = () => {
-  const ads = [];
-  for (let i = 0; i < SIMILAR_AD_COUNT; i++) {
-    ads.push({
-      autor: getAutor(i),
-      offer: getOffer(),
-      location: getLocation()
-    });
-  }
-  return ads;
-};
+const generateAds = (maxAds) =>  Array.from({ length: maxAds}, (_item, index) => createAd(index + 1));
 
 generateAds();
-
-/* Создаем массив из случайных уникальных ID длиной
-const getUniqueImgNumber = () => {
-  const imgNumber = usersId.splice(getRandomNumber(0, (usersId.length -1 )), 1);
-  return imgNumber < 10 && imgNumber > 0 ? `0${imgNumber}` : `${imgNumber}`;
-};
-
-const randomUsersId = [];
-
-for (let i = 0; i < SIMILAR_AD_COUNT; i++) {
-  const k = getUniqueImgNumber();
-  randomUsersId.push(k);
-}
-*/
